0183d0249fb0f7888bb29317cb8bddd3005491c6
ASHISH SAHU <93507589+ashgitit2003@users.noreply.github.com>
2024-04-28 11:59:24 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,8 @@
+public class add {    
+    public static void main(String[] args) {
+        int a = 200;
+        int b = 21;
+        int sum = a + b;
+        System.out.println(a + " + " + b + " = " + sum);
+    }
+}
